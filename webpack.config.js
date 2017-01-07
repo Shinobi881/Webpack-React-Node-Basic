4158e69681171218ced8fe735ac49e24e8710294
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-01-07 11:27:46 -0800

    Intialize and get started

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './index.js', // relative path to the root JS file in our app
+  output: {
+    filename: './bundle.js' // Where our transpiled code will end up (built by webpack)
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ } // Transpile JS and React 
+    ]
+  }
+};
